1b30ed1e0423465b48e6d50fc66b311fda7aec69
shoma-MAX <shomakatsura@gmail.com>
2021-02-08 18:03:29 +0900

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { MessageComponent } from './message/message.component';
+import { GetTimeComponent } from './get-time/get-time.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, MessageComponent ],
+  declarations: [ AppComponent, HelloComponent, MessageComponent, GetTimeComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/get-time/get-time.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-get-time',
+  templateUrl: './get-time.component.html',
+  styleUrls: ['./get-time.component.css']
+})
+export class GetTimeComponent implements OnInit {
+
+  constructor() { }
+  
+
+  ngOnInit() {
+    
+  }
+
+}
